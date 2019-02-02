bf012deab1ecaf46a86ad392730aada1d1b5c977
Daniil Gannota <daniil.gannota@gmail.com>
2019-02-02 16:23:34 +0000

    Added basic menu for the next stage

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,40 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     // Main method
     public static void main(String[] args) {
-        System.out.println("Hello World!\n");
-        new GitPMDAnalyser();
+        System.out.println("Hi there!");
+
+        menu();
+    }
+
+    private static void menu() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nSelect one of the operations:" +
+                "\n1. Use \"links.txt\" to analyse Git using PMD." +
+                "\n2. Use \"records.xlsx\" for Bugzilla." +
+                "\nPress \'0\' for exit.");
+        try {
+            int answer = scan.nextInt();
+            switch (answer) {
+                case 1:
+                    new GitPMDAnalyser();
+                    break;
+                case 2:
+                    System.out.println("This option isn't ready yet.");
+                    menu();
+                    break;
+                case 0:
+                    System.out.println("\nBye-bye!");
+                    break;
+                default:
+                    System.out.println("Wrong input, try again.");
+                    menu();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Wrong input, try again. Must be of integer type.");
+            menu();
+        }
     }
 }
